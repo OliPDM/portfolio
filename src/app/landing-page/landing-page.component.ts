d3387996714135a579bc3274f78f70fd9291c24a
Oliver Geschine <oli.geschine@web.de>
2026-01-28 18:19:01 +0100

    add landing-page | navbar | first icons | first structure

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [CommonModule],
-  templateUrl: './landing-page.component.html',
+  imports: [CommonModule, NavbarComponent],
+  template: `<section>
+      <div><app-navbar></app-navbar></div>
+  </section>`,
   styleUrl: './landing-page.component.scss'
 })
 export class LandingPageComponent {
