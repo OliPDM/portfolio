b806e0be612276c99bafcc025722723f4a556b90
Oliver Geschine <oli.geschine@web.de>
2026-02-01 18:54:21 +0100

    add more components | add style to landing-page

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -2,11 +2,15 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { AboutMeComponent } from './about-me/about-me.component';
+import { SkillsComponent } from './skills/skills.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { CommentsComponent } from './comments/comments.component';
+import { ContactComponent } from './contact/contact.component';
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [CommonModule, LandingPageComponent, AboutMeComponent],
+  imports: [CommonModule, LandingPageComponent, AboutMeComponent, SkillsComponent, PortfolioComponent, CommentsComponent, ContactComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
